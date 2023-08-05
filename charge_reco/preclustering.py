af7441d00a2576a6a6f0370f1ecf330eb1fad7b0
Fogarty <sfogarty@tur012.slac.stanford.edu>
2023-08-05 12:55:49 -0700

    Revert some code to old implementation, maybe not necessary

--- charge_reco/preclustering.py
+++ charge_reco/preclustering.py
@@ -16,32 +16,49 @@ def zip_pixel_tyz(packets,ts, pixel_xy, module):
     ## some of this code copied from larnd-sim
     with open(module.detprop_path) as df:
         detprop = yaml.load(df, Loader=yaml.FullLoader)
-    ts_inmm = v_drift*1e1*ts*0.1 # timestamp in us * drift speed in mm/us
+    #ts_inmm = v_drift*1e1*ts*0.1 # timestamp in us * drift speed in mm/us
 
     TPC_OFFSETS = np.array(detprop['tpc_offsets'])*10
     # Inverting x and z axes
     TPC_OFFSETS[:, [2, 0]] = TPC_OFFSETS[:, [0, 2]]
 
     module_to_io_groups = detprop['module_to_io_groups']
-    io_group = packets['io_group']
-    io_group_rel = np.copy(io_group)
-    io_group_rel[io_group_rel % 2 == 1] = 1
-    io_group_rel[io_group_rel % 2 == 0] = 2
-    io_channel = packets['io_channel']
-    chip_id = packets['chip_id']
-    channel_id = packets['channel_id']
-    keys = np.stack((io_group_rel, io_channel, chip_id, channel_id), axis=-1)
+    io_groups = packets['io_group']
+    io_groups_rel = np.copy(io_groups)
+    io_groups_rel[io_groups_rel % 2 == 1] = 1
+    io_groups_rel[io_groups_rel % 2 == 0] = 2
+    io_channels = packets['io_channel']
+    chip_ids = packets['chip_id']
+    channel_ids = packets['channel_id']
+    #keys = np.stack((io_group_rel, io_channel, chip_id, channel_id), axis=-1)
 
-    xyz_values = np.array([pixel_xy.get(tuple(key), [0.0, 0.0, 0.0,0.0]) for key in keys])
-
+    xyz_values = []
+    ts_inmm = []
+    packets_keep_mask = np.zeros(len(packets), dtype=bool)
+    for i in range(len(io_channels)):
+        io_group = io_groups_rel[i]
+        io_channel = io_channels[i]
+        chip_id = chip_ids[i]
+        channel_id = channel_ids[i]
+        
+        dict_values = pixel_xy.get((io_group, io_channel, chip_id, channel_id))
+        if dict_values is not None:
+            xyz_values.append(dict_values)
+            ts_inmm.append(v_drift*1e1*ts[i]*0.1)
+            packets_keep_mask[i] = True
+        #else:
+            #print(f'KeyError {(io_group, io_channel, chip_id, channel_id)}')
+    xyz_values = np.array(xyz_values)
+    ts_inmm = np.array(ts_inmm)
+    
     # adjust coordinates by TPC offsets, if there's >1 module
     io_group_grouped_by_module = list(module_to_io_groups.values())
     for i in range(len(io_group_grouped_by_module)):
         io_group_group = io_group_grouped_by_module[i]
-        if len(np.unique(io_group)) > 2:
-            xyz_values[(io_group == io_group_group[0]) | (io_group == io_group_group[1])] += np.concatenate((TPC_OFFSETS[i], np.array([0])))
+        if len(np.unique(io_groups)) > 2:
+            xyz_values[(io_groups == io_group_group[0]) | (io_groups == io_group_group[1])] += np.concatenate((TPC_OFFSETS[i], np.array([0])))
         else:
             pass
     txyz = np.hstack((ts_inmm[:, np.newaxis], xyz_values))
-    return txyz
+    return txyz, packets_keep_mask
 
