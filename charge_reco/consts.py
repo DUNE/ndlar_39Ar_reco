1c29ad118e7e6a24e04ef7a40f0f4185c98a881c
Fogarty <sfogarty@tur005.slac.stanford.edu>
2023-08-09 11:56:19 -0700

    Define EVENT_SEPARATOR

--- charge_reco/consts.py
+++ charge_reco/consts.py
@@ -33,4 +33,4 @@ clusters_dtype = np.dtype([('nhit', '<i4'), ('q', '<f8'),('io_group', '<i4'),\
     
 ext_trig_dtype = np.dtype([('unix', '<i8'), ('ts_PPS', '<i8'), ('io_group', '<i4')])
 
-
+EVENT_SEPARATOR='eventID'
