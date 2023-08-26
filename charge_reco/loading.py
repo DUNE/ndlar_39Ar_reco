8b4a956cca20fb615924e25365bb4a7b1c2e4472
sam-fogarty <samuel.fogarty@gmail.com>
2023-08-26 13:00:13 -0700

    Remove pickle dependence, get dict from yaml

--- charge_reco/loading.py
+++ charge_reco/loading.py
@@ -1,4 +1,3 @@
-from pickle import load as load_pickle
 from json import load as load_json
 from collections import defaultdict
 import yaml
@@ -17,11 +16,54 @@ def load_detector_properties(module):
         detprop = yaml.load(df, Loader=yaml.FullLoader)
     return detprop
 
-def load_geom_dict(geom_dict_path):
+def rotate_pixel(pixel_pos, tile_orientation):
+    return pixel_pos[0]*tile_orientation[2], pixel_pos[1]*tile_orientation[1]
+
+def load_geom_dict(module):
     ## load geometry dictionary for pixel positions
-    with open(geom_dict_path, "rb") as f_geom_dict:
-        geom_dict = load_pickle(f_geom_dict)
-    return geom_dict
+    # code copied from larpix-readout-parser
+    with open(module.detector_dict_path) as f_larpix:
+        geometry_yaml = yaml.load(f_larpix, Loader=yaml.FullLoader)
+
+    pixel_pitch = geometry_yaml['pixel_pitch']
+    chip_channel_to_position = geometry_yaml['chip_channel_to_position']
+    tile_orientations = geometry_yaml['tile_orientations']
+    tile_positions = geometry_yaml['tile_positions']
+    tile_indeces = geometry_yaml['tile_indeces']
+    xs = np.array(list(chip_channel_to_position.values()))[:, 0] * pixel_pitch
+    ys = np.array(list(chip_channel_to_position.values()))[:, 1] * pixel_pitch
+    x_size = max(xs) - min(xs) + pixel_pitch
+    y_size = max(ys) - min(ys) + pixel_pitch
+
+    geometry = defaultdict(dict)
+    
+    for tile in geometry_yaml['tile_chip_to_io']:
+        tile_orientation = tile_orientations[tile]
+
+        for chip_channel in geometry_yaml['chip_channel_to_position']:
+            chip = chip_channel // 1000
+            channel = chip_channel % 1000
+            try:
+                io_group_io_channel = geometry_yaml['tile_chip_to_io'][tile][chip]
+            except KeyError:
+                continue
+
+            io_group = io_group_io_channel // 1000 # io_group per module (not the real io_group)
+            io_channel = io_group_io_channel % 1000
+            x = chip_channel_to_position[chip_channel][0] * \
+                pixel_pitch + pixel_pitch / 2 - x_size / 2
+            y = chip_channel_to_position[chip_channel][1] * \
+                pixel_pitch + pixel_pitch / 2 - y_size / 2
+            
+            x, y = rotate_pixel((x, y), tile_orientation)
+
+            x += tile_positions[tile][2]
+            y += tile_positions[tile][1]
+            z = tile_positions[tile][0]
+            direction = tile_orientations[tile][0]
+
+            geometry[(io_group, io_channel, chip, channel)] = np.array([x, y, z, direction])
+    return geometry
 
 def load_pedestal_and_config(module):
     # function to open the pedestal and configuration files to get the dictionaries
