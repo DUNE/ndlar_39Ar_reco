9eb7f74e86924afffa52b75724c07407bc05ca69
sam-fogarty <samuel.fogarty@gmail.com>
2023-09-18 07:32:27 -0700

    Light event building w/ adc64 to h5 dump script, i.e. w/o flow

--- charge_reco/match_light_to_ext_triggers.py
+++ charge_reco/match_light_to_ext_triggers.py
@@ -8,13 +8,13 @@ from tqdm import tqdm
 import h5py
 import os
 
-def main(input_clusters_file, input_light_file, output_filename):
+def main(input_clusters_file, output_filename, *input_light_files):
     """
     # Args:
           input_clusters_file (str): path to file that contains charge clusters 
                 and external triggers processed with charge_clustering.py
-          input_light_file (str): path to file that contains ndlar_flow-processed 
-                light data
+          output_filename (str): path to hdf5 output file
+          input_light_files (str): paths to files that contain hdf5 files containing light data processed with adc64format
     """
     if os.path.exists(output_filename):
         raise Exception('Output file '+ str(output_filename) + ' already exists.')
@@ -27,51 +27,113 @@ def main(input_clusters_file, input_light_file, output_filename):
     ext_trig_unix = np.array(f_charge['ext_trig']['unix'])
     ext_trig_t = np.array(f_charge['ext_trig']['t'])
     
-    # get light events
-    f_light = h5py.File(input_light_file, 'r')
-    light_events = f_light['light/events/data']
-    light_wvfms = f_light['light/wvfm/data']
+    # get light events for each ADC
+    f_adc1 = h5py.File(input_light_files[0], 'r')
+    f_adc2 = h5py.File(input_light_files[1], 'r')
+    
+    # get timestamps for matching
+    tai_ns_adc1 = np.array(f_adc1['time']['tai_ns'])
+    unix_adc1 = (np.array(f_adc1['header']['unix']) * 1e-3).astype('i8')
+    tai_ns_adc2 = np.array(f_adc2['time']['tai_ns'])
+    unix_adc2 = (np.array(f_adc2['header']['unix']) * 1e-3).astype('i8')
+    tai_ns_tolerance = 1000
+    unix_tolerance = 1
     
     ts_window = 2000 # nsec
     unix_mask = np.zeros(len(ext_trig_unix), dtype=bool)
     tai_ns_mask = np.zeros(len(ext_trig_unix), dtype=bool)
     isMatch_mask = np.zeros(len(ext_trig_unix), dtype=bool)
 
-    num_light_events = int(len(light_events)/2) # len(light_events)
+    #num_light_events = int(len(light_events)/2) # len(light_events)
     #num_light_events = 10000
     light_events_matched = []
     light_wvfms_matched = []
     light_index = 0
     light_event_indices = np.zeros(len(clusters), dtype='i8')
     
-    # match between external triggers and light triggers
-    for i in tqdm(range(num_light_events), desc=' Matching external triggers to light events: '):
-        light_unix_s = int(np.unique(light_events[i]['utime_ms']*1e-3)[1])
-        light_tai_ns = int((np.unique(light_events[i]['tai_ns'])[1]) % 1e9)
-        isUnixMatch = ext_trig_unix == light_unix_s
-        isPPSMatch = (ext_trig_t > light_tai_ns - ts_window) & \
-            (ext_trig_t < light_tai_ns + ts_window)
-        unix_mask += isUnixMatch
-        tai_ns_mask += isPPSMatch
-        isMatch = isUnixMatch & isPPSMatch
-        isMatch_mask += isMatch
-        # keep only matched light events, and keep track of indices for associations
-        if np.sum(isMatch) == 1:
-            light_events_matched.append(light_events[i])
-            light_wvfms_matched.append(light_wvfms[i])
-            ext_trig_index = np.where(isMatch)[0]
-            np.put(light_event_indices, np.where(clusters['ext_trig_index'] == ext_trig_index)[0], light_index)
-            light_index += 1
+    light_events_dtype = np.dtype([('id', '<i4'), ('tai_ns', '<i8'), \
+                                   ('unix', '<i8'), ('channels_adc1', 'u1', (48,)), \
+                                   ('channels_adc2', 'u1', (48,)), \
+                                    ('voltage_adc1', 'i4', (48, 1000)), ('voltage_adc2', 'i4', (48, 1000))])
+    light_events_all = np.zeros((0,), dtype=light_events_dtype)
     
-    light_events_matched = np.array(light_events_matched, dtype=light_events.dtype)
-    light_wvfms_matched = np.array(light_wvfms_matched, dtype=light_wvfms.dtype)
+    batch_size = 500
+    batch_index = 0
+    first_batch = True
+    # match between external triggers and light triggers
+    for i in tqdm(range(len(tai_ns_adc1)), desc=' Matching external triggers to light events: '):
+        light_tai_ns_mask = (tai_ns_adc2 > tai_ns_adc1[i] - tai_ns_tolerance) & ((tai_ns_adc2 < tai_ns_adc1[i] + tai_ns_tolerance))
+        light_unix_mask = (unix_adc2 > unix_adc1[i] - unix_tolerance) & ((unix_adc2 < unix_adc1[i] + unix_tolerance))
+        light_match_mask = light_tai_ns_mask & light_unix_mask
+        
+        # only match to ext triggers if there is an event in each ADC
+        if np.sum(light_match_mask) == 1:
+            light_event = np.zeros((1,), dtype=light_events_dtype)
+            light_tai_ns = (float(tai_ns_adc1[i]) + float(f_adc2['time'][light_match_mask]['tai_ns'][0]))/2 
+            light_unix_s = unix_adc1[i]
+            
+            # match light trig to external triggers
+            isUnixMatch = ext_trig_unix == light_unix_s
+            isPPSMatch = (ext_trig_t > light_tai_ns - ts_window) & \
+                (ext_trig_t < light_tai_ns + ts_window)
+            unix_mask += isUnixMatch
+            tai_ns_mask += isPPSMatch
+            isMatch = isUnixMatch & isPPSMatch
+            isMatch_mask += isMatch
+            
+            # keep only matched light events, and keep track of indices for associations
+            if np.sum(isMatch) == 1:
+                # get data for event
+                data_adc1 = f_adc1['data'][f_adc1['ref'][i]['start']:f_adc1['ref'][i]['stop']]
+                data_adc2 = f_adc2['data'][f_adc2['ref'][light_match_mask][0]['start']:f_adc2['ref'][light_match_mask][0]['stop']]
+
+                # get channels and voltages
+                channels_adc1 = data_adc1['channel']
+                voltage_adc1 = data_adc1['voltage']
+                channels_adc2 = data_adc2['channel']
+                voltage_adc2 = data_adc2['voltage']
+
+                # save light event data to array
+                light_event['id'] = light_index
+                light_event['channels_adc1'] = channels_adc1
+                light_event['channels_adc2'] = channels_adc2
+                light_event['voltage_adc1'] = voltage_adc1
+                light_event['voltage_adc2'] = voltage_adc2
+                light_event['tai_ns'] = light_tai_ns
+                light_event['unix'] = light_unix_s
+                light_events_all = np.concatenate((light_events_all, light_event))
+                
+                ext_trig_index = np.where(isMatch)[0]
+                np.put(light_event_indices, np.where(clusters['ext_trig_index'] == ext_trig_index)[0], light_index)
+                light_index += 1
+                
+                if batch_index > batch_size and first_batch:
+                    first_batch = False
+                    with h5py.File(output_filename, 'a') as output_file:
+                        output_file.create_dataset('light_events', data=light_events_all, maxshape=(None,))
+                    light_events_all = np.zeros((0,), dtype=light_events_dtype)
+                    batch_index = 0
+                elif batch_index > batch_size and not first_batch:
+                    with h5py.File(output_filename, 'a') as output_file:
+                        output_file['light_events'].resize((output_file['light_events'].shape[0] + light_events_all.shape[0]), axis=0)
+                        output_file['light_events'][-light_events_all.shape[0]:] = light_events_all
+                    light_events_all = np.zeros((0,), dtype=light_events_dtype)
+                    batch_index = 0
+                else:
+                    batch_index += 1
+        # make sure to save last batch no matter what
+        if i == len(tai_ns_adc1)-1 and len(light_events_all) > 0:
+            with h5py.File(output_filename, 'a') as output_file:
+                output_file['light_events'].resize((output_file['light_events'].shape[0] + light_events_all.shape[0]), axis=0)
+                output_file['light_events'][-light_events_all.shape[0]:] = light_events_all
+    
     # get matched clusters
     ext_trig_mask = isMatch_mask
     total_matches = np.sum(ext_trig_mask)
-    print(f'Efficiency of light event matches = {total_matches/num_light_events}')
-    print(f'Efficiency of unix matches = {np.sum(unix_mask)/num_light_events}')
-    print(f'Efficiency of PPS matches = {np.sum(tai_ns_mask)/num_light_events}')
-    print(' ')
+    #print(f'Efficiency of light event matches = {total_matches/num_light_events}')
+    #print(f'Efficiency of unix matches = {np.sum(unix_mask)/num_light_events}')
+    #print(f'Efficiency of PPS matches = {np.sum(tai_ns_mask)/num_light_events}')
+    #print(' ')
     print(f'Efficiency of matching between ext triggers and light events = {total_matches/len(ext_trig_mask)}')
     print(f'Efficiency of unix matches = {np.sum(unix_mask)/len(ext_trig_mask)}')
     print(f'Efficiency of PPS matches = {np.sum(tai_ns_mask)/len(ext_trig_mask)}')
@@ -80,7 +142,6 @@ def main(input_clusters_file, input_light_file, output_filename):
     clusters_matched_mask = np.isin(ext_trig_indices, ext_trig_indices_matched)
     clusters_matched = clusters[clusters_matched_mask]
     light_event_indices = light_event_indices[clusters_matched_mask]
-    print(f'light_event_indices after mask: {np.unique(light_event_indices)}')
     # replace ext_trig_index with light event indices
     clusters_new = np.zeros_like(clusters_matched)
     for name in clusters_matched.dtype.names:
@@ -92,8 +153,6 @@ def main(input_clusters_file, input_light_file, output_filename):
     clusters_matched = clusters_new
     with h5py.File(output_filename, 'a') as output_file:
         output_file.create_dataset('clusters', data=clusters_matched)
-        output_file.create_dataset('light_events', data=light_events_matched)
-        output_file.create_dataset('light_wvfms', data=light_wvfms_matched)
     print(f'Saved output to {output_filename}')
     
 if __name__ == "__main__":
---
+++ util/run_light_event_building.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+DET="module-1"
+if [ "${DET}" = "module-0" ]; then
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco
+elif [ "${DET}" = "module-1" ]; then
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
+    data_folder=/global/cfs/cdirs/dune/www/data/Module1/LRS/SingleModule_Jan22
+    ADC_name_1=0cd913fb
+    ADC_name_2=0cd93db0
+    file_timestamp=20220208_130819
+elif [ "${DET}" = "module-2" ]; then
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco
+else
+    echo "Exiting as $DET is not a recognized run name"
+    exit 0
+fi
+
+current_dir=${pwd}
+LRS_file_1=${ADC_name_1}_${file_timestamp}
+LRS_file_2=${ADC_name_2}_${file_timestamp}
+output_file=${output_folder}/light_events_${file_timestamp}.h5
+
+shifter --image=mjkramer/sim2x2:genie_edep.3_04_00.20230620 --module=None -- /bin/bash << EOF
+set +o posix
+source /environment
+
+rm -rf convert.venv
+python3 -m venv convert.venv
+source convert.venv/bin/activate
+pip install --upgrade pip
+pip install -r ../requirements.txt
+
+python3 ./adc64format/adc64_to_hdf5.py ${data_folder}/${LRS_file_1}.data ${OUTDIR}/${LRS_file_1}.h5
+python3 ./adc64format/adc64_to_hdf5.py ${data_folder}/${LRS_file_2}.data ${OUTDIR}/${LRS_file_2}.h5
+EOF
+
+
+
