9e4a83fde07b1426c5839e1ca2bf3797bddbe7bd
sam-fogarty <samuel.fogarty@gmail.com>
2023-12-02 14:34:50 -0800

    Add input directory parameter

--- util/run_cluster_selection.sh
+++ util/run_cluster_selection.sh
@@ -2,20 +2,24 @@
 
 DET="ModuleX"
 if [ "${DET}" = "Module0" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco
 elif [ "${DET}" = "Module1" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
 elif [ "${DET}" = "Module2" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco
 elif [ "${DET}" = "ModuleX" ]; then
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
+	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco/60Co_source
 else
 	echo "Exiting as $DET is not a recognized run name"
 	exit 0
 fi
 
-INFILENAME_CLUSTERS=${OUTDIR}/packets-2023_10_04_11_56_CEST_clusters.h5
-OUTFILENAME=${OUTDIR}/clusters_selection_2023_10_04_11_56_CEST.h5
+INFILENAME_CLUSTERS=${INDIR}/packets-2023_10_06_04_40_CEST_clusters.h5
+OUTFILENAME=${OUTDIR}/clusters_selection_2023_10_06_04_40_CEST.h5
 
 shifter --image=mjkramer/sim2x2:genie_edep.3_04_00.20230620 --module=None -- /bin/bash << EOF
 set +o posix
