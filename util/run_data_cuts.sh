b60dd54972c9ecc53f474ba9a932ad468c0348a8
sam-fogarty <samuel.fogarty@gmail.com>
2023-12-06 13:41:16 -0800

    Add more module-1 files

---
+++ util/run_data_cuts.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+DET="module-1"
+if [ "${DET}" = "module-0" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco
+elif [ "${DET}" = "module-1" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
+elif [ "${DET}" = "module-2" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco
+elif [ "${DET}" = "module-X" ]; then
+	INDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
+else
+	echo "Exiting as $DET is not a recognized run name"
+	exit 0
+fi
+
+file_paths=(
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_01_47_59_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_02_08_00_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_02_28_01_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_03_54_19_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_04_14_21_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_04_34_22_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_04_54_23_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_05_14_25_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_08_54_32_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_09_14_33_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_09_34_35_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_12_48_18_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_13_08_19_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_13_57_39_CET.h5"
+    "${INDIR}/charge-light-matched-clusters_2022_02_08_14_17_41_CET.h5"
+    
+)
+
+module load python
+cd ../charge_reco
+python3 apply_data_cuts.py ${DET} "${file_paths[@]}"
