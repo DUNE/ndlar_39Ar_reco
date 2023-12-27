6ac489504facb92ec4167c8e29605b4a98cfbce8
sam-fogarty <samuel.fogarty@gmail.com>
2023-12-27 12:32:26 -0800

    Update input config

--- charge_reco/input_config.py
+++ charge_reco/input_config.py
@@ -11,7 +11,7 @@ class ModuleConfig:
             self.data_type = 'data'
             self.detector_dict_path = 'charge_layout/module0_multi_tile_layout-2.3.16.yaml'
             self.detprop_path = 'detector_properties/module0.yaml'
-            self.use_disabled_channels_list = True
+            self.use_disabled_channels_list = False
             self.disabled_channels_list = 'disabled_channels/module0_disabled_channels_noise_cut.npz'
             self.pedestal_file = 'pedestal/module0_datalog_2021_04_02_19_00_46_CESTevd_ped.json'
             self.config_file = 'config/module0_evd_config_21-03-31_12-36-13.json'
@@ -24,11 +24,13 @@ class ModuleConfig:
             self.nBatches = 400
             self.batches_limit = 400
             self.ext_trig_matching_tolerance_unix = 1
-            self.ext_trig_matching_tolerance_PPS = 1.5e3 # ns
+            self.ext_trig_matching_tolerance_PPS = 2e3 # ns
             self.charge_light_matching_lower_PPS_window = 150000
             self.charge_light_matching_upper_PPS_window = full_drift_time + 150000
             self.charge_light_matching_unix_window = 0
             self.ext_trig_PPS_window = 1000
+            self.samples = 256
+            self.nchannels = 58
 
         elif self.module_name == 'module-0_MC':
             self.detector = 'module-0'
@@ -80,6 +82,7 @@ class ModuleConfig:
             self.charge_light_matching_unix_window = 0
             self.ext_trig_PPS_window = 1000
             self.samples = 1000
+            self.nchannels = 58
             
         elif self.module_name == 'module-2':
             self.detector = 'module-2'
@@ -105,6 +108,8 @@ class ModuleConfig:
             self.charge_light_matching_unix_window = 0
             self.ext_trig_PPS_window = 1000
             self.samples = 1024
+            self.nchannels = 58
+            
         elif self.module_name == 'module-X':
             self.detector = 'module-X'
             self.data_type = 'data'
