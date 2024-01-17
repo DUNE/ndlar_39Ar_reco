c7c2784b03adea18541fb19c9f35acaf5118ca58
sam-fogarty <samuel.fogarty@gmail.com>
2024-01-16 22:23:46 -0800

    Update matching shell script

--- util/run_charge-light-matching.sh
+++ util/run_charge-light-matching.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-DET="module-1"
+DET="module-2"
 if [ "${DET}" = "module-0" ]; then
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco
+	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco/39Ar_reco
 elif [ "${DET}" = "module-1" ]; then
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
+elif [ "${DET}" = "module-2" ]; then
+        OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco/39Ar_reco
 elif [ "${DET}" = "module-X" ]; then
         OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco/nominalHV_beforeElevatedHV
 else
@@ -12,10 +14,10 @@ else
 	exit 0
 fi
 
-INFILENAME_CLUSTERS=${OUTDIR}/clusters_selection_2023_10_04_19_37_CEST.h5
-INFILENAME_LIGHT_1=${OUTDIR}/0cd8d631_20231004_193740.h5
-INFILENAME_LIGHT_2=${OUTDIR}/0cd913fa_20231004_193740.h5
-OUTFILENAME=${OUTDIR}/charge-light-matched-clusters_2023_10_04_19_37_CEST.h5
+INFILENAME_CLUSTERS=${OUTDIR}/self_trigger_tpc12_run2_tile3-packet-2022_12_03_13_52_CET_clusters.h5
+INFILENAME_LIGHT_1=${OUTDIR}/0cd8d631_20221203_135230.h5
+INFILENAME_LIGHT_2=${OUTDIR}/0cd913fa_20221203_135230.h5
+OUTFILENAME=${OUTDIR}/charge-light-matched-clusters_2022_12_03_13_52_CET.h5
 
 shifter --image=mjkramer/sim2x2:genie_edep.3_04_00.20230620 --module=None -- /bin/bash << EOF
 set +o posix
@@ -29,7 +31,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 cd charge_reco
-python3 match_light_to_ext_triggers.py ${INFILENAME_CLUSTERS} ${OUTFILENAME} ${INFILENAME_LIGHT_1} ${INFILENAME_LIGHT_2} --input_config_name ${DET}
+python3 match_light_to_clusters.py ${INFILENAME_CLUSTERS} ${OUTFILENAME} ${INFILENAME_LIGHT_1} ${INFILENAME_LIGHT_2} --input_config_name ${DET}
 EOF
 
 
