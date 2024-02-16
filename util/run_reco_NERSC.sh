636aa99fe1ff88d9c92cd35737df8c2d92a4538e
sam-fogarty <samuel.fogarty@gmail.com>
2024-02-16 12:11:39 -0800

    1) Now can run multiple files in a row 2) Remove shifter option, utilize module load python on NERSC for setup

--- util/run_reco_NERSC.sh
+++ util/run_reco_NERSC.sh
@@ -1,49 +1,65 @@
 #!/usr/bin/env bash
 
-DET="module-2"
+DET="module-0"
 if [ "${DET}" = "module-0" ]; then
-	data_folder=/global/cfs/cdirs/dune/www/data/Module0/TPC1+2/dataRuns/packetData
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco/39Ar_reco
+    data_folder=/global/cfs/cdirs/dune/www/data/Module0/TPC1+2/dataRuns/packetData
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_reco/39Ar_reco
+    file_name_start=datalog_
 elif [ "${DET}" = "module-1" ]; then
-	data_folder=/global/cfs/cdirs/dune/www/data/Module1/TPC12/dataRuns/packetData
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
+    data_folder=/global/cfs/cdirs/dune/www/data/Module1/TPC12/dataRuns/packetData
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
+    file_name_start=packet_
 elif [ "${DET}" = "module-2" ]; then
-	data_folder=/global/cfs/cdirs/dune/www/data/Module2/packetized/TPC12_run2
-        #PEDESTAL_FOLDER=/global/cfs/cdirs/dune/www/data/Module2/packetized/TPC12_run2
-	PEDESTAL_FOLDER=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco/39Ar_reco
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco/39Ar_reco
+    data_folder=/global/cfs/cdirs/dune/www/data/Module2/packetized/TPC12_run2
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco/39Ar_reco
+    file_name_start=self_trigger_tpc12_run2-packet-
 elif [ "${DET}" = "module-X" ]; then
     data_folder=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
-	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco
 elif [ "${DET}" = "SingleCube" ]; then
-        #data_folder=/global/cfs/cdirs/dune/www/data/ModuleX/commission
-        data_folder=/global/cfs/cdirs/dune/users/sfogarty/Bern_SC_reco
-        OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Bern_SC_reco
+    #data_folder=/global/cfs/cdirs/dune/www/data/ModuleX/commission
+    data_folder=/global/cfs/cdirs/dune/users/sfogarty/Bern_SC_reco
+    OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Bern_SC_reco
 else
-	echo "Exiting as $DET is not a recognized run name"
-	exit 0
+    echo "Exiting as $DET is not a recognized run name"
+    exit 0
 fi
 
-PKT_FILENAME=self_trigger_tpc12_run2_tile3-packet-2022_12_03_13_52_CET
-#PKT_FILENAME=datalog_2021_04_04_10_38_37_CEST
-INFILENAME=${data_folder}/${PKT_FILENAME}.h5
-OUTFILENAME=${OUTDIR}/${PKT_FILENAME}_clusters.h5
-PEDESTAL_FILE=${PEDESTAL_FOLDER}/pedestal_run2_tpc12_prc_4096_HV_off-binary-2022_12_02_23_34_CET_packets.h5
+current_dir=${pwd}
 
-shifter --image=mjkramer/sim2x2:genie_edep.3_04_00.20230620 --module=None -- /bin/bash << EOF
-set +o posix
-source /environment
-
+TIMESTAMP_CHARGE=(
+            "2021_04_04_09_38_27_CEST"
+            "2021_04_04_09_58_31_CEST"
+            "2021_04_04_10_38_37_CEST"
+            "2021_04_04_11_18_41_CEST"
+            "2021_04_04_11_38_43_CEST"
+            "2021_04_04_12_18_48_CEST"
+            "2021_04_04_13_18_54_CEST"
+            "2021_04_04_13_38_56_CEST"
+            "2021_04_04_14_19_01_CEST"
+            "2021_04_04_14_39_03_CEST"
+            "2021_04_04_15_19_07_CEST"
+            "2021_04_04_16_59_17_CEST"
+            "2021_04_04_17_19_19_CEST"
+            "2021_04_04_17_39_22_CEST"
+            "2021_04_04_17_59_24_CEST"
+            )
+            
+list_length=${#TIMESTAMP_CHARGE[@]}
 cd ..
-rm -rf convert.venv
-python3 -m venv convert.venv
-source convert.venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-
+#rm -rf convert.venv
+#python3 -m venv convert.venv
+#source convert.venv/bin/activate
+#pip install --upgrade pip
+#pip install -r requirements.txt
 cd charge_reco
-python3 charge_clustering.py ${DET} ${INFILENAME} ${OUTFILENAME} --save_hits=True --match_to_ext_trig=False
+module load python # nersc specific
+for ((i = 0; i < list_length; i++)); do
+    timestamp="${TIMESTAMP_CHARGE[i]}"
+    PKT_FILENAME=${file_name_start}${timestamp}
+    INFILENAME=${data_folder}/${PKT_FILENAME}.h5
+    OUTFILENAME=${OUTDIR}/${PKT_FILENAME}_clusters.h5
+    python3 charge_clustering.py ${DET} ${INFILENAME} ${OUTFILENAME} --save_hits=True --match_to_ext_trig=False
+done
 
-EOF
-#--pedestal_file=${PEDESTAL_FILE} --vcm_dac=71 --vref_dac=217
 
