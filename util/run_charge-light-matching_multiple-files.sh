e914c9cca406e7158c043f4ea76b1b9544c007bb
sam-fogarty <samuel.fogarty@gmail.com>
2024-02-23 07:51:44 -0800

    Update matching shell script

--- util/run_charge-light-matching_multiple-files.sh
+++ util/run_charge-light-matching_multiple-files.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-DET="module0_run2"
+DET="module0_run1"
 if [ "${DET}" = "module0_run1" ]; then
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_run1_reco/39Ar_reco
     ADC_1_NAME=0a7a314c
     ADC_2_NAME=0a7b54bd
     PACKET_FILE_NAME_START=datalog_
+    data_folder=/global/cfs/cdirs/dune/www/data/Module0/LRS
 elif [ "${DET}" = "module0_run2" ]; then
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module0_run2_reco/39Ar_reco
-    ADC_1_NAME=0a7a314c
-    ADC_2_NAME=0a7b54bd
-    PACKET_FILE_NAME_START=packet_
+        ADC_1_NAME=0a7a314c
+        ADC_2_NAME=0a7b54bd
+        PACKET_FILE_NAME_START=packet_
+	data_folder=/global/cfs/cdirs/dune/www/data/Module0-Run2/LRS
 elif [ "${DET}" = "module1" ]; then
 	OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module1_reco/39Ar_reco_09132023
     ADC_1_NAME=0cd913fb
     ADC_2_NAME=0cd93db0
     PACKET_FILE_NAME_START=packet_
+    data_folder=/global/cfs/cdirs/dune/www/data/Module1/LRS/SingleModule_Jan22
 elif [ "${DET}" = "module2" ]; then
         OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/Module2_reco/39Ar_reco
         ADC_1_NAME=0cd8d631
         ADC_2_NAME=0cd913fa
 	PACKET_FILE_NAME_START=self_trigger_tpc12_run2-packet-
+	data_folder=/global/cfs/cdirs/dune/www/data/Module2/LRS.2/cosmic
 elif [ "${DET}" = "moduleX" ]; then
         OUTDIR=/global/cfs/cdirs/dune/users/sfogarty/ModuleX_reco/nominalHV_beforeElevatedHV
 else
@@ -29,77 +33,77 @@ else
 fi
 
 # module 0 run 1
-#TIMESTAMP_CHARGE=(
-#            "2021_04_04_09_38_27_CEST"
-#            "2021_04_04_09_58_31_CEST"
-#            "2021_04_04_10_38_37_CEST"
-#            "2021_04_04_11_18_41_CEST"
-#            "2021_04_04_11_38_43_CEST"
-#            "2021_04_04_12_18_48_CEST"
-#            "2021_04_04_13_18_54_CEST"
-#            "2021_04_04_13_38_56_CEST"
-#            "2021_04_04_14_19_01_CEST"
-#            "2021_04_04_14_39_03_CEST"
-#            "2021_04_04_15_19_07_CEST"
-#            "2021_04_04_16_59_17_CEST"
-#            "2021_04_04_17_19_19_CEST"
-#            "2021_04_04_17_39_22_CEST"
-#            "2021_04_04_17_59_24_CEST"
-#            )
-#TIMESTAMP_LIGHT=(
-#            "20210404_093828"
-#            "20210404_095833"
-#            "20210404_103840"
-#            "20210404_111841"
-#            "20210404_113847"
-#            "20210404_121849"
-#            "20210404_131854"
-#            "20210404_133856"
-#            "20210404_141859"
-#            "20210404_143903"
-#            "20210404_151909"
-#            "20210404_165920"
-#            "20210404_171920"
-#            "20210404_173921"
-#            "20210404_175925"
-#            )
+TIMESTAMP_CHARGE=(
+            "2021_04_04_09_38_27_CEST"
+            "2021_04_04_09_58_31_CEST"
+            "2021_04_04_10_38_37_CEST"
+            "2021_04_04_11_18_41_CEST"
+            "2021_04_04_11_38_43_CEST"
+            "2021_04_04_12_18_48_CEST"
+            "2021_04_04_13_18_54_CEST"
+            "2021_04_04_13_38_56_CEST"
+            "2021_04_04_14_19_01_CEST"
+            "2021_04_04_14_39_03_CEST"
+            "2021_04_04_15_19_07_CEST"
+            "2021_04_04_16_59_17_CEST"
+            "2021_04_04_17_19_19_CEST"
+            "2021_04_04_17_39_22_CEST"
+            "2021_04_04_17_59_24_CEST"
+            )
+TIMESTAMP_LIGHT=(
+            "20210404_093828"
+            "20210404_095833"
+            "20210404_103840"
+            "20210404_111841"
+            "20210404_113847"
+            "20210404_121849"
+            "20210404_131854"
+            "20210404_133856"
+            "20210404_141859"
+            "20210404_143903"
+            "20210404_151909"
+            "20210404_165920"
+            "20210404_171920"
+            "20210404_173921"
+            "20210404_175925"
+            )
 
 # module 0 run 2
-TIMESTAMP_CHARGE=(
-         #"2021_06_23_12_42_01_CEST"
-	     "2021_06_23_13_02_02_CEST"
-	     "2021_06_23_13_22_03_CEST"
-	     "2021_06_23_13_42_04_CEST"
-	     "2021_06_23_14_02_05_CEST"
-	     "2021_06_23_14_22_06_CEST"
-	     "2021_06_23_14_42_07_CEST"
-	     "2021_06_23_15_02_08_CEST"
-	     "2021_06_23_15_22_09_CEST"
-	     "2021_06_23_15_42_10_CEST"
-	     "2021_06_23_16_02_11_CEST"
-	     "2021_06_23_16_22_12_CEST"
+#TIMESTAMP_CHARGE=(
+#             "2021_06_23_12_42_01_CEST"
+	     #"2021_06_23_13_02_02_CEST"
+	     #"2021_06_23_13_22_03_CEST"
+	     #"2021_06_23_13_42_04_CEST"
+	     #"2021_06_23_14_02_05_CEST"
+	     #"2021_06_23_14_22_06_CEST"
+	     #"2021_06_23_14_42_07_CEST"
+	     #"2021_06_23_15_02_08_CEST"
+	     #"2021_06_23_15_22_09_CEST"
+	     #"2021_06_23_15_42_10_CEST"
+	     #"2021_06_23_16_02_11_CEST"
+	     #"2021_06_23_16_22_12_CEST"
 	     #"2021_06_23_16_42_13_CEST"
 	     #"2021_06_23_17_02_14_CEST"
 	     #"2021_06_23_17_22_15_CEST"
-	)
+#	)
 
-TIMESTAMP_LIGHT=(
-	#"20210623_124201"
-	"20210623_130207"
-	"20210623_132205"
-	"20210623_134209"
-	"20210623_140208"
-	"20210623_142208"
-	"20210623_144208"
-	"20210623_150209"
-	"20210623_152217"
-    "20210623_154232"
-	"20210623_160212"
-	"20210623_162213"
+#TIMESTAMP_LIGHT=(
+#	"20210623_124201"
+	##"20210623_130207"
+	#"20210623_132205"
+	#"20210623_134209"
+	#"20210623_140208"
+	#"20210623_142208"
+	#"20210623_144208"
+	#"20210623_150209"
+	#"20210623_152217"
+        #"20210623_154232"
+	#"20210623_160212"
+	#"20210623_162213"
 	#"20210623_164213"
 	#"20210623_170215"
 	#"20210623_172216"
-	)
+#	)
 
 # module 1
 #TIMESTAMP_CHARGE=(
@@ -120,7 +124,7 @@ TIMESTAMP_LIGHT=(
 #            "2022_02_08_14_17_41_CET"
 #            )
 #TIMESTAMP_LIGHT=(
-#            "20220208_014759"
+#	    "20220208_014759"
 #            "20220208_020800"
 #            "20220208_022802"
 #            "20220208_035419"
@@ -130,39 +134,42 @@ TIMESTAMP_LIGHT=(
 #            "20220208_051425"
 #            "20220208_085432"
 #            "20220208_091433"
-#            "20220208_093435"
+#	    "20220208_093435"
 #            "20220208_124818"
 #            "20220208_130819"
 #            "20220208_135739"
 #            "20220208_141741"
 #            )
 
+# module 2
 #TIMESTAMP_CHARGE=(
-#            "2022_12_02_00_07_CET"
-#            "2022_12_02_01_38_CET"
-#            "2022_12_02_01_48_CET"
-#            "2022_12_02_01_58_CET"
-#            "2022_12_02_02_08_CET"
-#	    "2022_12_02_02_18_CET"
-#	    "2022_12_02_02_28_CET"
-#	    "2022_12_02_02_38_CET"
-#	    "2022_12_02_02_48_CET"
-#	    #"2022_12_02_02_58_CET"
-#	    #"2022_12_02_03_08_CET"
-#	    #"2022_12_02_03_18_CET"
-#	    #"2022_12_02_03_28_CET"
-#           )  
+#        "2022_12_02_14_00_CET"
+#        "2022_12_02_14_10_CET"
+#        "2022_12_02_14_20_CET"
+#        "2022_12_02_14_30_CET"
+#        "2022_12_02_14_40_CET"
+#        "2022_12_02_14_50_CET"
+# 	"2022_12_02_15_00_CET"
+#        "2022_12_02_15_10_CET"
+#	"2022_12_02_15_20_CET"
+#	"2022_12_02_15_30_CET"
+#        "2022_12_02_15_50_CET"	
+#	)  
 #TIMESTAMP_LIGHT=(
-#        "20221202_000743"
-#	"20221202_013841"
-#	"20221202_014842"
-#	"20221202_015844"
-#	"20221202_020845"
-#	"20221202_021847"
-#	"20221202_022848"
-#	"20221202_023849"
-#	"20221202_024851"
+#	"20221202_140028"
+#	"20221202_141029"
+#	"20221202_142031"
+#	"20221202_143032"
+#	"20221202_144034"
+# 	"20221202_145035"
+#	"20221202_150036"
+#	"20221202_151038"
+#	"20221202_152039"
+#	"20221202_153041"
+#	"20221202_155044"
 #	)
+
+
 list_length=${#TIMESTAMP_CHARGE[@]}
 
 cd ..
@@ -172,13 +179,13 @@ source convert.venv/bin/activate
 #pip install --upgrade pip
 #pip install -r requirements.txt
 
-cd charge_reco
+cd reco
 for ((i = 0; i < list_length; i++)); do
     timestamp_charge="${TIMESTAMP_CHARGE[i]}"
     timestamp_light="${TIMESTAMP_LIGHT[i]}"
     INFILENAME_CLUSTERS=${OUTDIR}/${PACKET_FILE_NAME_START}${timestamp_charge}_clusters.h5
-    INFILENAME_LIGHT_1=${OUTDIR}/${ADC_1_NAME}_${timestamp_light}.h5
-    INFILENAME_LIGHT_2=${OUTDIR}/${ADC_2_NAME}_${timestamp_light}.h5
+    INFILENAME_LIGHT_1=${data_folder}/${ADC_1_NAME}_${timestamp_light}.data
+    INFILENAME_LIGHT_2=${data_folder}/${ADC_2_NAME}_${timestamp_light}.data
     OUTFILENAME=${OUTDIR}/charge-light-matched-clusters_${timestamp_charge}.h5
 
     python3 match_light_to_clusters.py ${INFILENAME_CLUSTERS} ${OUTFILENAME} ${INFILENAME_LIGHT_1} ${INFILENAME_LIGHT_2} --input_config_name ${DET}
